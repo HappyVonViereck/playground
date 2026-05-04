a0e821262e56ab0177eb4eda18935c2151a3a3fd
sVierkant <sarah-monique.vierkant@bvb.lavie-reha.de>
2026-05-04 11:16:47 +0200

    beim clicken wird jetz das geklickte feld farbig

--- composeApp/src/androidMain/kotlin/com/happy/vonviereck/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/happy/vonviereck/MainActivity.kt
@@ -5,8 +5,11 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,6 +28,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.*
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
@@ -33,6 +37,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
@@ -50,11 +55,15 @@ class GameViewModel : ViewModel() {
     val allTiles = mutableListOf<Tile>()
     var currentPath = mutableStateOf<List<Tile>>(emptyList())
     val maus = Maus()
-    val germanCheese= cheeseGerman()
+    val germanCheese = cheeseGerman()
     val tileSize = 40.dp
-    var everythingisLoaded=false
+    var everythingisLoaded = false
 }
 
+object btnStyle {
+    val bgColor = Color(0xFF91F5AD)
+    val fontColor = Color(0xFF031926)
+}
 // ─── Activity ────────────────────────────────────────────────────────────────
 
 class MainActivity : ComponentActivity() {
@@ -72,40 +81,40 @@ class MainActivity : ComponentActivity() {
 fun App(vm: GameViewModel = viewModel()) {
 
     LaunchedEffect(vm.allTiles.size) {
-if(vm.allTiles.size>=100) {
-    vm.maus.moveTo(7, 5, vm.allTiles)
-    vm.germanCheese.placeCheeseOnFreeTileRandom(vm)
-    Log.d("Debug", "Startpositionen gesetzt")
-}
-    }
-
-    Box(modifier = Modifier.fillMaxSize()){
-        mainScreen()
-    Box(modifier = Modifier.padding(top = 150.dp))
-        {
-        Column(
-            modifier = Modifier
-                .padding(top = 80.dp)
-                .align(Alignment.TopCenter),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            dropDownMenu()
-            Row(horizontalArrangement = Arrangement.Start) {
-                createSavebtn(vm.allTiles)
-                changetilesBtn(vm.allTiles)
-                knopf(vm)
-                käsePlatzierenTestbtn(vm)
-            }
-            createTileSet(vm)
-
+        if (vm.allTiles.size >= 100) {
+            vm.maus.moveTo(7, 5, vm.allTiles)
+            vm.germanCheese.placeCheeseOnFreeTileRandom(vm)
+            Log.d("Debug", "Startpositionen gesetzt")
         }
     }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        mainScreen()
+        Box(modifier = Modifier.padding(top = 150.dp)) {
+            Column(
+                modifier = Modifier
+                    .padding(top = 80.dp)
+                    .align(Alignment.TopCenter),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                dropDownMenu()
+                Row(horizontalArrangement = Arrangement.Start) {
+                    createSavebtn(vm.allTiles)
+                    changetilesBtn(vm.allTiles)
+                }
+                Row(horizontalArrangement = Arrangement.Start) {
+                    knopf(vm)
+                    käsePlatzierenTestbtn(vm)
+                }
+                createTileSet(vm)
+
+            }
+        }
         // hier alle anderen elemente die globale position brauchen
         vm.maus.createMaus()
         vm.germanCheese.createCheese()
+    }
 }
-}
-
 
 
 // ─── Buttons ─────────────────────────────────────────────────────────────────
@@ -121,98 +130,110 @@ fun createSavebtn(allTiles: MutableList<Tile>) {
     var levelName by remember { mutableStateOf("") }
 
     Button(
-        onClick =
-            {
-                Log.d("Btn", "es wurde auf speichern gedrückt ")
-                showSaveDialog = true  //Trigger für den SpeicherAlert(DialogBox)
-            })
-    { Text("Speichern") }
+        onClick = {
+            Log.d("Btn", "es wurde auf speichern gedrückt ")
+            showSaveDialog = true  //Trigger für den SpeicherAlert(DialogBox)
+        }, colors = ButtonDefaults.buttonColors(
+            containerColor = btnStyle.bgColor
+
+        )
+    ) { Text("Speichern", color = btnStyle.fontColor) }
 
 
     // Speichern-Dialog
     if (showSaveDialog) { //wenn der trigger von knopf auf true ist wird der alert ausgelöst
-        AlertDialog(
-            onDismissRequest = {
+        AlertDialog(onDismissRequest = {
+            showSaveDialog = false
+            levelName = ""
+        }, title = { Text("Level benennen") }, text = {
+            OutlinedTextField(
+                value = levelName,
+                onValueChange = { levelName = it },
+                label = { Text("Level-Name") },
+                singleLine = true,
+                placeholder = { Text("z.B. level1") })
+        }, confirmButton = { //Der knopf zum bestätigen wurde gedrückt
+            Button(
+                onClick = {
+                    if (levelName.isNotBlank()) {
+                        loader.saveGrid(context, allTiles, levelName)
+                        showSaveDialog = false
+                        levelName = "" //leert die textbox damit man beim nächten levelbennen
+                        // eine freie Textbox hat
+                    }
+                }) { Text("Speichern") }
+        }, dismissButton = {
+            OutlinedButton(onClick = {
                 showSaveDialog = false
-                levelName = ""
-            },
-            title = { Text("Level benennen") },
-            text = {
-                OutlinedTextField(
-                    value = levelName,
-                    onValueChange = { levelName = it },
-                    label = { Text("Level-Name") },
-                    singleLine = true,
-                    placeholder = { Text("z.B. level1") })
-            },
-            confirmButton = { //Der knopf zum bestätigen wurde gedrückt
-                Button(
-                    onClick = {
-                        if (levelName.isNotBlank()) {
-                            loader.saveGrid(context, allTiles, levelName)
-                            showSaveDialog = false
-                            levelName = "" //leert die textbox damit man beim nächten levelbennen
-                                           // eine freie Textbox hat
-                        }
-                    }) { Text("Speichern") }
-            },
-            dismissButton =
-                {
-                    OutlinedButton(onClick = { showSaveDialog = false })//Der Vorgang wird abgerochen
-                    { Text("Abbrechen") }
-                })
+            })//Der Vorgang wird abgerochen
+            { Text("Abbrechen") }
+        })
     }
 }
 
 @Composable
-fun gridLadenButtons(allTiles: MutableList<Tile>, enabledState: MutableState<Boolean>, titel: MutableState<String>)
- {
+fun gridLadenButtons(
+    allTiles: MutableList<Tile>, enabledState: MutableState<Boolean>, titel: MutableState<String>
+) {
     val context = LocalContext.current
     val loader = LaoderForTile()
     var savedLevels by remember { mutableStateOf(loader.getAllSavedLevels(context)) }
 
     savedLevels.forEach { label ->
-        DropdownMenuItem(
-            text = {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = label)
+        var isPressed by remember { mutableStateOf(false) }
+        val interactionSource = remember { MutableInteractionSource() }
+        isPressed = interactionSource.collectIsPressedAsState().value
 
-                    // Delete-Button
-                    IconButton(onClick = {
-                        loader.deleteGrid(context, label)
-                        savedLevels = loader.getAllSavedLevels(context) // Liste aktualisieren
-                    }) {
-                        Icon(
-                            imageVector = Icons.Default.Delete,
-                            contentDescription = "Löschen",
-                            tint = MaterialTheme.colorScheme.error
-                        )
-                    }
+        DropdownMenuItem(
+            modifier = Modifier
+                .background(
+                    if (isPressed) btnStyle.bgColor
+                    else Color.Transparent
+                ),
+            interactionSource = interactionSource,
+            text = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = label, color = btnStyle.fontColor)
+
+                // Delete-Button
+                IconButton(onClick = {
+                    loader.deleteGrid(context, label)
+                    savedLevels = loader.getAllSavedLevels(context) // Liste aktualisieren
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Löschen",
+                        tint = MaterialTheme.colorScheme.error
+                    )
                 }
-            },
-            onClick = {
-                loader.loadGrid(context, label, allTiles)
-                enabledState.value = false
-                titel.value = label
             }
-        )
+        }, onClick = {
+            loader.loadGrid(context, label, allTiles)
+            enabledState.value = false
+            titel.value = label
+        })
     }
 }
-@Composable
-fun changetilesBtn(allTiles: MutableList<Tile>){
 
-    Button(onClick = {
-        allTiles.forEach { tile->
-            tile.isInEditMode=!tile.isInEditMode
-        }
-        Log.d("Btn", "es wurde in den Bearbeitungsmodus gewechelts ")
-    })
-    {
-        Text("bearbeiten")
+@Composable
+fun changetilesBtn(allTiles: MutableList<Tile>) {
+
+    Button(
+        onClick = {
+            allTiles.forEach { tile ->
+                tile.isInEditMode = !tile.isInEditMode
+            }
+            Log.d("Btn", "es wurde in den Bearbeitungsmodus gewechelts ")
+        }, colors = ButtonDefaults.buttonColors(
+            containerColor = btnStyle.bgColor
+
+        )
+    ) {
+        Text("Bearbeiten", color = btnStyle.fontColor)
     }
 }
 
@@ -220,18 +241,22 @@ fun changetilesBtn(allTiles: MutableList<Tile>){
 fun knopf(vm: GameViewModel) {
 
     var start = remember { mutableStateOf(false) }
-    Button(onClick = {
-        Log.d("Debug", "Knopf zum testen wurde gedrückt")
-        start.value = true
+    Button(
+        onClick = {
+            Log.d("Debug", "Knopf zum testen wurde gedrückt")
+            start.value = true
+        }, colors = ButtonDefaults.buttonColors(
+            containerColor = btnStyle.bgColor
+
+        )
+    ) {
+        Text("Test1", color = btnStyle.fontColor)
     }
-    ){
-        Text("Käse finden")
-    }
-    bewegenMaustest(start,vm)
+    bewegenMaustest(start, vm)
     goAPath(vm, vm.currentPath.value)
 }
 
-fun bewegenMaustest(start: MutableState<Boolean>,vm: GameViewModel){
+fun bewegenMaustest(start: MutableState<Boolean>, vm: GameViewModel) {
     if (start.value) {
 
         val aTile = vm.maus.sucheTile(7, 5, vm.allTiles)
@@ -240,13 +265,18 @@ fun bewegenMaustest(start: MutableState<Boolean>,vm: GameViewModel){
         vm.currentPath.value = createAPath(aTile, bTile, vm)
         start.value = false
     }
-    }
+}
 
 
 @Composable
-fun käsePlatzierenTestbtn(vm: GameViewModel){
-    Button(onClick = { vm.germanCheese.placeCheeseOnFreeTileRandom(vm)}){
-        Text("käse los")
+fun käsePlatzierenTestbtn(vm: GameViewModel) {
+    Button(
+        onClick = { vm.germanCheese.placeCheeseOnFreeTileRandom(vm) },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = btnStyle.bgColor
+        )
+    ) {
+        Text("Test2", color = btnStyle.fontColor)
     }
 }
 
@@ -254,47 +284,48 @@ fun käsePlatzierenTestbtn(vm: GameViewModel){
 
 @Composable
 fun createTileSet(vm: GameViewModel) {
-        HorizontalGrid( 10, 10,  vm.allTiles, vm.maus,vm)
+    HorizontalGrid(10, 10, vm.allTiles, vm.maus, vm)
 }
 
 @Composable
-fun HorizontalGrid(rows: Int, columns: Int, allTiles: MutableList<Tile>, maus: Maus,vm: GameViewModel) {
+fun HorizontalGrid(
+    rows: Int, columns: Int, allTiles: MutableList<Tile>, maus: Maus, vm: GameViewModel
+) {
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
+    ) {
 
-            repeat(rows) { row ->
-                Row {
-                    repeat(columns) { col ->
-                        val tileBlock = remember(row, col) {
-                            Tile().also { tile ->
-                                tile.xCord = col
-                                tile.yCord = row
+        repeat(rows) { row ->
+            Row {
+                repeat(columns) { col ->
+                    val tileBlock = remember(row, col) {
+                        Tile().also { tile ->
+                            tile.xCord = col
+                            tile.yCord = row
 
-                                // Rand-Tiles als Mauer setzen
-                                if (col == 0 || col == columns - 1 || row == 0 || row == rows - 1) {
-                                    tile.toggleTile()
-                                }
-                                allTiles.add(tile)
+                            // Rand-Tiles als Mauer setzen
+                            if (col == 0 || col == columns - 1 || row == 0 || row == rows - 1) {
+                                tile.toggleTile()
                             }
+                            allTiles.add(tile)
                         }
+                    }
 
-                        Card(
-                            colors = CardDefaults.cardColors(
-                                containerColor = MaterialTheme.colorScheme.onSurfaceVariant
-                            ),
-                            shape = CutCornerShape(0.dp),
-                            modifier = Modifier.size(vm.tileSize.value.dp)
-                        ) {
-                            tileBlock.createATile()
-                        }
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.onSurfaceVariant
+                        ),
+                        shape = CutCornerShape(0.dp),
+                        modifier = Modifier.size(vm.tileSize.value.dp)
+                    ) {
+                        tileBlock.createATile()
                     }
                 }
             }
         }
     }
+}
 
 //───────DropDownMenu ─────────────────────────────────────────────────────────────
 @Composable
@@ -371,17 +402,18 @@ fun createAPath(tileA: Tile?, tileB: Tile?, vm: GameViewModel): List<Tile> {
         if (tile != null) path.add(tile)
     }
 
-    path.forEach { t->
-        Log.d("path","path besteht aus Tile(${t.xCord},${t.yCord})")
+    path.forEach { t ->
+        Log.d("path", "path besteht aus Tile(${t.xCord},${t.yCord})")
     }
 
     return path
 }
-@Composable
-fun goAPath(vm: GameViewModel,l:List<Tile>){
 
-        LaunchedEffect(l) {
-            l.forEach { tile ->
+@Composable
+fun goAPath(vm: GameViewModel, l: List<Tile>) {
+
+    LaunchedEffect(l) {
+        l.forEach { tile ->
             vm.maus.moveTo(tile.xCord, tile.yCord, vm.allTiles)
             delay(500L)
         }
---
+++ composeApp/src/androidMain/kotlin/com/happy/vonviereck/victoryScreen.kt
@@ -0,0 +1,12 @@
+package com.happy.vonviereck
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+
+@Composable
+
+fun victoryScreen(){
+    Box(){
+
+    }
+}
